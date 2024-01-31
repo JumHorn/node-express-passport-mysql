1285dbe1d1a2d0c37d57e7210d597071138b279a
JumHorn <JumHorn@gmail.com>
2024-01-31 08:45:13 +0000

    listen to localhost

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 58081;
 
 var passport = require('passport');
 var flash = require('connect-flash');
@@ -44,5 +44,5 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
+app.listen(port,'127.0.0.1');
 console.log('The magic happens on port ' + port);
