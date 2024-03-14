79042dfe8a36412d880cf7b545880412052ed735
JumHorn <JumHorn@gmail.com>
2024-03-14 11:08:25 +0800

    use mysql2

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // config/connection.js
 
 // load up the user model
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var dbconfig = require('./database');
 var connection = mysql.createPool(dbconfig.connection);
 
